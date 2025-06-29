506da60b8c5f849c15be7b248f25509fa28af151
vikky-chauhan <vikaspht123@gmail.com>
2025-06-29 22:00:50 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,11 +1,1 @@
-# vikas.py
-import sys
-
-def say_hello():
-    if len(sys.argv) > 1:
-        user_name = sys.argv[1]
-    else:
-        user_name = "DefaultUser"
-    print(f"Hello, {user_name}! Welcome to GitHub from Jenkins.")
-
-say_hello()
+print("Hello Buddy !! V7")
